3e848adc96c498ef2a873b6536646440aba02e3a
Daniel Keyes <danielkeyes@Daniels-MacBook-Pro.local>
2021-06-21 16:58:43 -0500

    update colors to white stars on black

--- app/src/main/java/dev/danielkeyes/starfield/ui/theme/Color.kt
+++ app/src/main/java/dev/danielkeyes/starfield/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package dev.danielkeyes.starfield.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val primary = Color(0xFF424242)
+val primaryVariant = Color(0xFF1B1B1B)
+val secondary = Color(0xFF311B92)
